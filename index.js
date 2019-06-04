46ff7e292212a1f103eda253f8ee5f7aa4368785
Julian Trebach <juliantreb@gmail.com>
2019-06-04 01:12:00 +0100

    i

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
 const express = require("express");
+const path =require ("path");
+//const logger = require("./src/utilities/middleware/logger");
 
+//Init express:
 const app =express();
 
-var properties = new Array();
+//Middleware:
+//app.use(logger);
+
+//Body Parser Middlware:
+//var properties = new Array();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+// app.use("/api/users", require("./src/api/users-routes"));
+// app.use("/api/auth", require("./src/api/auth-routes"));
+
+
+const usersRouter = require("./user/index");
+app.use("/api/users", usersRouter);
+
+app.get("/api/users", (req, res) => {
     console.log(req.headers);
     res.send("<p>Hello</p>");
 });
 
-app.get("/properties", (req, res) => {
-    // var properties = new Array();
-    // properties.push({
-    //     id: 1,
-    //     name: "One",
-    //     location: "Lisbon",
-    // });
-    res.json(properties);
-});
+// app.get("/properties", (req, res) => {
+//     // var properties = new Array();
+//     // properties.push({
+//     //     id: 1,
+//     //     name: "One",
+//     //     location: "Lisbon",
+//     // });
+//     res.json(properties);
+// });
 
-app.post("/properties", (req, res) => {
-    const property = req.body;
-    properties.push(property);
-    // res.send();
-    res.json(property);
-});
+// app.post("/properties", (req, res) => {
+//     const property = req.body;
+//     properties.push(property);
+//     // res.send();
+//     res.json(property);
+// });
 
 app.listen(3000, (err) =>{
     if(err) {
